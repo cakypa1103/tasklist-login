a601bb7932afc49ce89f687a7d525f83f40cecd6
takahiro.yasuda <cakypa1103@gmail.com>
2019-03-17 10:05:13 +0000

    syuusei

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,11 +9,6 @@
         <div class="col-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
                 <div class="form-group">
-                    {!! Form::label('title', 'タイトル:') !!}
-                    {!! Form::text('title', '', ['class' => 'form-control']) !!}
-                </div>    
-                
-                <div class="form-group">
                     {!! Form::label('content', '内容:') !!}
                     {!! Form::text('content', '', ['class' => 'form-control']) !!}
                 </div>
